87b6dae7993a37fe7ede2886b440ee278a88c1ac
sasasubotic <54679567+sasasubotic@users.noreply.github.com>
2021-06-08 13:18:10 -0400

    FF

--- particles.js
+++ particles.js
@@ -1,11 +1,4 @@
-/* -----------------------------------------------
-/* Author : Vincent Garreau  - vincentgarreau.com
-/* MIT license: http://opensource.org/licenses/MIT
-/* Demo / Generator : vincentgarreau.com/particles.js
-/* GitHub : github.com/VincentGarreau/particles.js
-/* How to use? : Check the GitHub README
-/* v2.0.0
-/* ----------------------------------------------- */
+
 
 particlesJS("particles-js", {
   "particles": {
@@ -119,4 +112,5 @@ particlesJS("particles-js", {
 });
 
 requestAnimationFrame();
+
 
