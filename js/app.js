9a879dc6dfc1b4cb8a1514f81ea46996ad143064
sasasubotic <sasasubotic78@gmail.com>
2024-11-01 09:49:43 -0400

    Refreshing for year.

--- js/app.js
+++ js/app.js
@@ -1,117 +1,14 @@
-const canvas = document.querySelector("#canvas");
-const ctx = canvas.getContext("2d");
 
-let w, h, balls = [];
-let mouse = {
-	x: undefined,
-	y: undefined
-}
-let rgb = [
-	"rgb(26, 125, 156)",
-	"rgb(46, 99, 113)",
-	"rgb(52, 88, 177)",
-	"rgb(155, 89, 182)",
-	"rgb(241, 196, 115)",
-	"rgb(230, 126, 34)",
-	"rgb(231, 76, 160)"
-]
-
-function init() {
-	resizeReset();
-	animationLoop();
-}
-
-function resizeReset() {
-	w = canvas.width = window.innerWidth;
-	h = canvas.height = window.innerHeight;
-}
-
-function animationLoop() {
-	ctx.clearRect(0, 0, w, h);
-	ctx.globalCompositeOperation = 'lighter';
-	drawBalls();
-
-	let temp = [];
-	for (let i = 0; i < balls.length; i++) {
-		if (balls[i].time <= balls[i].ttl) {
-			temp.push(balls[i]);
-		}
-	}
-	balls = temp;
-
-	requestAnimationFrame(animationLoop);
-}
-
-function drawBalls() {
-	for (let i = 0; i < balls.length; i++) {
-		balls[i].update();
-		balls[i].draw();
-	}
-}
-
-function mousemove(e) {
-	mouse.x = e.x;
-	mouse.y = e.y;
-
-	for (let i = 0; i < 1; i++) {
-		balls.push(new Ball());
-	}	
-}
-
-function mouseout() {
-	mouse.x = undefined;
-	mouse.y = undefined;
-}
-
-function getRandomInt(min, max) {
-	return Math.round(Math.random() * (max - min)) + min;
-}
-
-function easeOutQuart(x) {
-	return 1 - Math.pow(1 - x, 4);
-}
-
-class Ball {
-	constructor() {
-		this.start = {
-			x: mouse.x + getRandomInt(-20, 20),
-			y: mouse.y + getRandomInt(-20, 20),
-			size: getRandomInt(15, 12)
-		}
-		this.end = {
-			x: this.start.x + getRandomInt(-300, 300),
-			y: this.start.y + getRandomInt(-300, 300)
-		}
-
-		this.x = this.start.x;
-		this.y = this.start.y;
-		this.size = this.start.size;
-
-		this.style = rgb[getRandomInt(0, rgb.length - 1)];
-
-		this.time = 0;
-		this.ttl = 120;
-	}
-	draw() {
-		ctx.fillStyle = this.style;
-		ctx.beginPath();
-		ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-		ctx.closePath();
-		ctx.fill();
-	}
-	update() {
-		if (this.time <= this.ttl) {
-			let progress = 1 - (this.ttl - this.time) / this.ttl;
-
-			this.size = this.start.size * (1 - easeOutQuart(progress));
-			this.x = this.x + (this.end.x - this.x) * 0.01;
-			this.y = this.y + (this.end.y - this.y) * 0.01;
-		}
-		this.time++;
-	}
-}
-
-window.addEventListener("DOMContentLoaded", init);
-window.addEventListener("resize", resizeReset);
-window.addEventListener("mousemove", mousemove);
-window.addEventListener("mouseout", mouseout);
+document.addEventListener("DOMContentLoaded", () => {
+    const landingPage = document.getElementById("landing-page");
+    
+    // Add event listener to shift page left on click
+    landingPage.addEventListener("click", () => {
+        landingPage.classList.add("shift-left");
+        
+        // Redirect after animation (adjust time if needed for animation)
+        setTimeout(() => {
+            window.location.href = "home.html";
+        }, 600);
+    });
+});
